cd5ad27ded11519ffc12328c35d3b3efb902200b
wp-web-wiki <wp.web.wiki@gmail.com>
2023-06-26 23:12:55 +0900

    delete files

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-import "destyle.css";
 import "../scss/style.scss";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
-import "./event-cancel";
-import "./animation";
-import "./sound";
-
